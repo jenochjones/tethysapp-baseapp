2ee0f1007d7994a76c26f2525a407449a0882761
jonjones <jon.enoch.jones@gmail.com>
2020-07-30 16:47:38 -0700

    Initial commit

---
+++ tethysapp/baseapp/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class Baseapp(TethysAppBase):
+    """
+    Tethys app class for BASEIC APP.
+    """
+
+    name = 'BASEIC APP'
+    index = 'baseapp:home'
+    icon = 'baseapp/images/icon.gif'
+    package = 'baseapp'
+    root_url = 'baseapp'
+    color = '#d35400'
+    description = ''
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='baseapp',
+                controller='baseapp.controllers.home'
+            ),
+        )
+
+        return url_maps
