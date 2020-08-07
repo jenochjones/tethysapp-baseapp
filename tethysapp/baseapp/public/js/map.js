c9f5d61b88edd0a2bd191a665e13b6b16e106c93
jenochjones <46875087+jenochjones@users.noreply.github.com>
2020-08-07 07:07:57 -0600

    Update map.js

--- tethysapp/baseapp/public/js/map.js
+++ tethysapp/baseapp/public/js/map.js
@@ -1,10 +1,42 @@
+let firstlayeradded = false;
 let mapObj = map();
 let basemapObj = basemaps();
-let layerControlObj = L.control.layers(basemapObj).addTo(mapObj);
+let dataLayerObj = data_layer();
+let layerControlObj = L.control.layers(basemapObj,{'CHIRPS':dataLayerObj}).addTo(mapObj);
+
+/* Add functionality to the map object 
+boxZoom: true ... allows holding shift to draw a rectangle and zoom
+fullscreenControl: true,  ... button to full screen
+zoomSnap: .5,
+minZoom: 2,
+How can we add a datepicker in the animation slider, or do we have to settle for creating
+some kind of date packer in the app navigation menus.
+*/
 
 function map() {
-  return map = L.map('map').setView([37.75, -122.23], 10);
+  return map = L.map('map', {
+    center: [0, 0],
+    zoom: 3,
+    minZoom: 2,
+    zoomSnap: .5,
+    boxZoom: true,
+    maxBounds: L.latLngBounds(L.latLng(-100.0, -270.0), L.latLng(100.0, 270.0)),
+    timeDimension: true,
+    timeDimensionControl: true,
+    timeDimensionControlOptions: {
+      position: "bottomleft",
+      autoPlay: true,
+      loopButton: true,
+      backwardButton: true,
+      forwardButton: true,
+      timeSliderDragUpdate: true,
+      minSpeed: 2,
+      maxSpeed: 6,
+      speedStep: 1,
+    },
+  })
 }
+
 
 function basemaps() {
   // create the basemap layers
@@ -25,41 +57,54 @@ function basemaps() {
   return {
     "ESRI Imagery (No Label)": L.layerGroup([esri_imagery]).addTo(mapObj),
     "ESRI Imagery (Labeled)": L.layerGroup([esri_imagery, esri_labels]),
-    "ESRI Terrain": L.layerGroup([esri_terrain]),
-    "ESRI Topographic": L.layerGroup([esri_topo]),
-    "ESRI NationalGeographic": L.layerGroup([esri_ng]),
-    "ESRI Oceans": L.layerGroup([esri_oceans]),
-    "ESRI Streets": L.layerGroup([esri_streets]),
-    "ESRI Gray": L.layerGroup([esri_gray]),
-    "ESRI DarkGray": L.layerGroup([esri_darkgray]),
-    "ESRI ImageryClarity": L.layerGroup([esri_imageryclarity]),
-    "ESRI ImageryFirefly": L.layerGroup([esri_imageryfirefly]),
-    "ESRI ShadedRelief": L.layerGroup([esri_shadedrelief]),
-    "ESRI USATopo": L.layerGroup([esri_usatopo]),
-    "ESRI Physical": L.layerGroup([ esri_physical]),
+    "ESRI Terrain": L.layerGroup([esri_terrain, esri_labels]),
+    "ESRI Topographic": L.layerGroup([esri_topo, esri_labels]),
+    "ESRI NationalGeographic": L.layerGroup([esri_ng, esri_labels]),
+    "ESRI Oceans": L.layerGroup([esri_oceans, esri_labels]),
+    "ESRI Streets": L.layerGroup([esri_streets, esri_labels]),
+    "ESRI Gray": L.layerGroup([esri_gray, esri_labels]),
+    "ESRI DarkGray": L.layerGroup([esri_darkgray, esri_labels]),
+    "ESRI ImageryClarity": L.layerGroup([esri_imageryclarity, esri_labels]),
+    "ESRI ImageryFirefly": L.layerGroup([esri_imageryfirefly, esri_labels]),
+    "ESRI ShadedRelief": L.layerGroup([esri_shadedrelief, esri_labels]),
+    "ESRI USATopo": L.layerGroup([esri_usatopo, esri_labels]),
+    "ESRI Physical": L.layerGroup([ esri_physical, esri_labels]),
   }
 }
 
 
+function data_layer() {
+  // let layer = $('#select-var').val();
+  let layer = $("#variables").val();
+  let cs_rng = bounds[layer]; // from bounds.js
+  let cmap = cmaps[layer]; // from bounds.js
+  
+  if (firstlayeradded == true) {
+    mapObj.removeLayer(dataLayerObj);
+  }
 
-//mapbox map///////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*stylesheet and script files included in the javascript
-var mapObj = map();
-var baselayerObj = baselayer();
-
-function map() {
-  return L.map('map').setView([51.505, -0.09], 13);
+  const wmsurl = 'https://thredds.servirglobal.net/thredds/wms/climateserv/ucsb-chirps-gefs/global/0.05deg/10dy/ucsb-chirps-gefs.daily.2020.nc4';
+  const wmsLayer = L.tileLayer.wms(wmsurl, {
+    layers: layer,
+    dimension: 'time',
+    useCache: true,
+    crossOrigin: false,
+    format: 'image/png',
+    transparent: true,
+    opacity: 0.8,
+    BGCOLOR: '0x000000',
+    styles: cmap,
+    colorscalerange: cs_rng,
+  });
+ 
+  wmsLayerTime = L.timeDimension.layer.wms(wmsLayer, {
+    name: 'time',
+    requestTimefromCapabilities: true,
+    updateTimeDimension: true,
+    updateTimeDimensionMode: 'replace',
+    cache: 20,
+  });
+  firstlayeradded = true;
+  return wmsLayerTime.addTo(mapObj);
+  
 }
-
-function baselayer() {
-  return L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoiam9uZXNqOTMiLCJhIjoiY2tkYWduemwwMGM2ODJwdDE4bDk2endqYyJ9.1aWa11kAevAcpD_ozur_0Q'
-  }).addTo(mapObj);
-}
-*/
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
