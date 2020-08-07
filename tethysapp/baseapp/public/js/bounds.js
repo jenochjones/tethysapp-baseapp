e401e6026d26b9dbe16c90f5c9ba840668554f7b
jenochjones <46875087+jenochjones@users.noreply.github.com>
2020-08-07 07:09:59 -0600

    Create bounds.js

---
+++ tethysapp/baseapp/public/js/bounds.js
@@ -0,0 +1,3 @@
+/* Define the bounds for the data variables */
+const bounds = {'precipitation_amount':'0,250', 'precipitation_anomaly':'-100,100'};
+const cmaps  = {'precipitation_amount': 'boxfill/rainbow', 'precipitation_anomaly':'boxfill/redblue'}
